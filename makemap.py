752ad9b4c7ce18e482afaefc720343a8b91d26c1
kudouakira <akkey12291996@gmail.com>
2017-01-21 20:41:21 +0900

    chenge drivers name

--- makemap.py
+++ makemap.py
@@ -6,11 +6,16 @@ from raspimouse_ros.srv import *
 from raspimouse_ros.msg import *
 from std_msgs.msg import UInt16
 
+P=1.0 #P control
+Base=300 #motor hz
+ideal_r= #ideal value for right walls
+ideal_l= #ideal value for left walls
+
 def raw_control(left_hz,right_hz):
-    pub = rospy.Publisher('/raspimouse/motor_raw', LeftRightFreq, queue_size=10)
+    pub = rospy.Publisher('/motor_raw', MotorFreqs, queue_size=10)
 
     if not rospy.is_shutdown():
-        d = LeftRightFreq()
+        d = MotorFreqs()
         d.left = left_hz
         d.right = right_hz
         pub.publish(d)
@@ -23,9 +28,7 @@ def lightsensor_callback(data):
 
 def oneframe(p,dis):
     r=2.4
-    t=(400*dis)/(2*3.14*r*p)
-    raw_control(p,p)
-    time.sleep(round(t,1))
+    t=(400*dis)/(2*3.14*r*p) #one frame 18cm
 
 def turn(p, deg, rorl): #rorl = -1(right) or 1(left)
     r=2.4
@@ -39,18 +42,34 @@ def turn(p, deg, rorl): #rorl = -1(right) or 1(left)
         raw_control(-p,p)
         time.sleep(round(t,1))
     else:
-        print "##cki# R or L please!!"
+        print "#u#kin R or L please!!"
 
 
 def stop_motor():
     raw_control(0,0)
     switch_motors(Fales)
 
+def wall_trace(ls):
+    E=P*(ls.left_side - ideal + ls.right_side - ideal)/2
+    if(ls.left_side < ):
+        wall_trace_right(ls,ideal)
+
+def wall_trace_left(ls):
+    E=P*(ls.left_side - ideal_l)
+    raw_control(Base + E, Base - E)
+
+def wall_trace_right(ls):
+    E=P*(ls.right_side - ideal_r)
+    raw_control(Base - E, Base + E)
+
+def wall_trace(ls)
+    E=P*(ls.left_side - ideal_l + ls.right_side - ideal_r)
+
 if __name__ == '__main__':
-    rospy.init_node('one_frame')
-#    sub = rospy.Subscriber('/raspimouse/lightsensors', LightSensorValues, lightsensor_callback)
+    rospy.init_node('makemap')
+    sub = rospy.Subscriber('/lightsensors', LightSensorValues, lightsensor_callback)
     raw_control(0,0)
     time.sleep(0.5)
-    oneframe(400)
-    turn(300, 90, -1) 
-    raw_control(0,0)
+#    oneframe(400)
+#    turn(300, 90, -1) 
+#    raw_control(0,0)
