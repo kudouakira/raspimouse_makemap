2a03cf0a9095592bf6c9d2f44f9d257f10894051
kudouakira <akkey12291996@gmail.com>
2017-01-23 04:36:22 +0900

    2017/01/23

--- raspimose_mapmaking.py
+++ raspimose_mapmaking.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*- 
-
+# -*- coding: utf-8 -*- 
 import sys, time
 import itertools
 import rospy
@@ -13,12 +12,17 @@ from std_msgs.msg import Header
 from nav_msgs.msg import MapMetaData, OccupancyGrid
 from geometry_msgs.msg import Pose, Point, Quaternion
 
+
 class createmaping(object):
     def __init__(self):
         rospy.init_node('makemap')
         self.__init__rviz()
         self.raw_control(0,0)
-        rospy.sleep(1)
+        rospy.sleep(0.5)
+        self.P = rospy.get_param('~P_gain',0.2)
+        self.p = rospy.get_param('Base_hz',400)
+        self.ideal_l = rospy.get_param('ideal_value_leftwall',800)
+        self.ideal_r = rospy.get_param('ideal_value_rightwall',800)
         sub = rospy.Subscriber('/raspimouse/lightsensors', LightSensorValues, self.lightsensor_callback, queue_size = 10)
         sub2 = rospy.Subscriber('/raspimouse/switches', Switches, self.switch_callback, queue_size = 10)
         self.sensor = [0,0,0,0]
@@ -27,6 +31,13 @@ class createmaping(object):
         rospy.loginfo("start")
 
     def lightsensor_callback(self, msg, val = 800): #vel change to map
+        #lightsensors value
+        self.left_side = mag.left_side
+        self.left_forward = msg.left_forward
+        self.right_side = msg.right_side
+        self.right_forward = msg.right_forward
+
+        #mapping judge
         if msg.right_forward > val: self.sensor[2] = True
         else : self.sensor[2] = False
         if msg.right_side > val: self.sensor[3] = True
@@ -52,23 +63,23 @@ class createmaping(object):
         self.pub2.publish(L)
         
     def oneframe(self,p,dis, r = 2.4):
-        t=(400*dis)/(2*3.14*r*p) #one frame 18cm
-        self.raw_control(p,p)
-        time.sleep(round(t,1))
+        self.t=(400*dis)/(2*3.14*r*p) #one frame 18cm
+        #self.raw_control(p,p)
+        #time.sleep(round(t,1))
 
     def turn(self,p, deg, rorl, r=2.4, a=5.0, rl=0): #rorl = -1(right) or 1(left)
-        t=(deg*400*a)/(360*r*p)
+        t2=(deg*400*a)/(360*r*p)
         if(rl > rorl):
             self.raw_control(p,-p)
-            time.sleep(round(t,1))
+            time.sleep(round(t2,1))
         elif(rl < rorl):
             self.raw_control(-p,p)
-            time.sleep(round(t,1))
-        else: print "#u#kin R or L please!!"
+            time.sleep(round(t2,1))
+        else: print "#u#kin -1(R) or 1(L) please!!"
 
     def stop_motor(self):
         self.raw_control(0,0)
-        switch_motors(Fales)
+        time.sleep(0.5)
 
     def recognition(self, type = 0):
         if self.sensor[0] and self.sensor[1] and self.sensor[2] and not self.sensor[3]: type = 1
@@ -82,12 +93,44 @@ class createmaping(object):
         #print type
         return type
 
+    def wall_trace_left(self): #traced left walls
+        E=self.P*(self.left_side - self.ideal_l)
+        self.raw_control(self.p + E, self.p - E)
+
+    def wall_tarce_right(self): #traced right walls
+        E=self.p*(self.right_side - self.ideal_r)
+        self.raw_control(self.p - E, se:f.p + E)
+
+    def wall_trace(self): #used 'wall_trace_left' & 'wall_tarce_right'
+        if self.left_side < self.right_side :
+            while count <= round(self.t,1):
+                self.wall_trace_left()
+                count += 0.1
+        elif self.left_side > self.right_side :
+            while count <= round(self.t,1):
+                self.wall_tarce_right()
+                count += 0.1
+
+    def search_rout(self):
+        self.turn(self.p,90,-1)
+        deg -= 90
+        if (self.left_forward + self.right_forward)*0.5 < 1000:
+            self.wall_trace()
+        self.turn(self.p,180,1)
+        deg += 180
+        if (self.left_forward + self.right_forward)*0.5 < 1000:
+            self.wall_trace()
+
     def run(self, deg=0,x=0,y=0): #main
         while not rospy.is_shutdown():
-            if self.switch[0]: #straiht
-                self.oneframe(400,9)
-                self.raw_control(0,0)
-                time.sleep(0.5)
+            if (self.left_side + self.left_forward + self.right_side + self.right_forward)*0.25 < 300:
+                self.stop_motor
+                break
+            elif (self.left_forward + self.right_forward)*0.5 < 1000:
+                self.oneframe(self.p,18)
+                print round(self.t,1)
+                self.wall_trace()
+
                 ##error
                 if deg+90 < 0: deg = 270
                 if deg+90 == 0:x+=3
@@ -101,17 +144,8 @@ class createmaping(object):
                 #error
                 self.rviz(self.recognition(),y,x,90+deg) #y,x,deg
 
-            elif self.switch[1]:
-                self.turn(300, 90, -1)
-                self.raw_control(0,0)
-                time.sleep(0.5)
-                deg -= 90
-
-            elif self.switch[2]:
-                self.turn(300, 90, 1)
-                self.raw_control(0,0)
-                time.sleep(0.5)
-                deg += 90
+            elif (self.left_forward + self.right_forward)*0.5 > 1000:
+                self.search_rout()
 
     def __init__rviz(self):#rviz__init__
         self.pub5 = rospy.Publisher('raspi_mouce', OccupancyGrid, queue_size=10)
